2898d5a7a86a2017fb440c723cbcc00f34353df7
waleweewe12 <m17661weerapast@gmail.com>
2020-01-12 12:59:46 +0700

    pass

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const express=require('express')
-const cors=require('cors')
-const bodyparser=require('body-parser')
 
-const app=express();
-app.use(express.json())
-app.use(cors())
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false }));
+// Importing Modules
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+// importing files
+const routes = require('./routes');
+
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 8080; // Step 1
+
+
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+    useNewUrlParser: true
+});
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Step 3
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
@@ -16,13 +33,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.get('/getuser',(req,res)=>{
-    res.send({
-        username:'weerapath'
-    })
-})
-
-const port = 5000
-app.listen(5000,()=>{
-    console.log("Application start at port "+port)
-})
+app.listen(PORT, () => {
+    log(`Server is starting at PORT: ${PORT}`);
+});
