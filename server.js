b06ad29b80e655417f379569b5ef6cac442da299
waleweewe12 <m17661weerapast@gmail.com>
2020-01-12 19:36:43 +0700

    add

--- server.js
+++ server.js
@@ -1,46 +1,38 @@
-/*settings */
-const express=require('express')
-const bodyparser=require('body-parser')
-const cors=require('cors')
-const mongoose=require('mongoose')
-const path=require('path')
 
-require('dotenv').config();
+// Importing Modules
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-mongoose.connect('mongodb+srv://smile69narak:'+process.env.MONGO_ATLAS_PASSWORDS+'@cluster0-m70hg.mongodb.net/test?retryWrites=true&w=majority',
-{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology: true
-})
-const app=express();
-app.use(cors());
-app.use(express.json());
+// importing files
+const routes = require('./routes');
 
-/*working */
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 5000; // Step 1
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}))
-const userRouter=require('./api/routes/user_route');
-const todoRouter=require('./api/routes/todo_route');
-app.use('/user',userRouter);
-app.use('/todo',todoRouter);
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+    useNewUrlParser: true
+});
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
     });
 }
 
-
-/*Connection */
-
-const connection = mongoose.connection
-connection.once('open',()=>{
-    console.log("Mongoose Connection!!")
-})
-
-const port=process.env.PORT || 8080
-app.listen(port,()=>{
-    console.log("application start at port "+port)
-})
+app.listen(PORT, () => {
+    log(`Server is starting at PORT: ${PORT}`);
+});
